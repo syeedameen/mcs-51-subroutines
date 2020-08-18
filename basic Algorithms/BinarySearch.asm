a5de256dd9bcaf8de124e3eb8f6b10c65d926080
Syeed Mohd Ameen <68741942+syeedameen@users.noreply.github.com>
2020-08-18 07:45:26 +0530

    Update BinarySearch.asm

--- basic Algorithms/BinarySearch.asm
+++ basic Algorithms/BinarySearch.asm
@@ -1,6 +1,3 @@
-;               BINARY SEARCH
-
-
 ; Error (To Create lo <= hi)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -38,99 +35,99 @@ BINSEARCH:
     POP 0X7F 
     POP 0X7E 
 
-    POP DPH     ;Base Addr. of Array  
-    POP DPL 
+    POP DPH                         ;Base Addr. of Array  
+    POP DPL                     
 
-    POP ACC     ;key element in r0 reg.
-    MOV R0,A 
+    POP ACC                         ;key element in r0 reg.
+    MOV R0,A                    
 
-    POP ACC     ;no. of element 
-    MOV R1,A 
+    POP ACC                         ;no. of element 
+    MOV R1,A                    
 
-    PUSH 0X7E 
-    PUSH 0X7F 
+    PUSH 0X7E                   
+    PUSH 0X7F                   
 
 
-    MOV R2,DPL  ;save datapointer reg.
-    MOV R3,DPH 
+    MOV R2,DPL                      ;save datapointer reg.
+    MOV R3,DPH                  
 
-    MOV R4,#0X00 ;define lo: and hi:
-    MOV R5,A 
+    MOV R4,#0X00                     ;define lo: and hi:
+    MOV R5,A                    
 
-    ; First calculating mid for calling subroutine 
+;---------------First calculating mid for calling subroutine-----------------
 
-    MOV A,R4    ;push lo: & hi: for mid cal
+    MOV A,R4                        ;push lo: & hi: for mid cal
     PUSH ACC 
     MOV A,R5 
     PUSH ACC
 
     ACALL MIDCAL_BINSEARCH     
-    POP ACC     ;get mid into Accumulator
-    MOV R6,A    ;temp store mid 
+    POP ACC                         ;get mid into Accumulator
+    MOV R6,A                        ;temp store mid 
     
-    ; check lo: <= hi:   
-    ; if condition false searching is unsuccessful
-    ; return 0x0000 address that is elemet not found 
+                                    ; check lo: <= hi:   
+                                    ; if condition false searching is unsuccessful
+                                    ; return 0x0000 address that is elemet not found 
 START_BINSEARCH:
-    MOV A,R4    ;mov acc = lo:
-    SUBB A,R5   ;test lo: with hi:
+    MOV A,R4                        ;mov acc = lo:
+    SUBB A,R5                       ;test lo: with hi:
     JC REPEAT_BINSEARCH
-    MOV A,R4    ;jump if lo: = hi:
+    MOV A,R4                        ;jump if lo: = hi:
     SUBB A,R5 
     JZ REPEAT_BINSEARCH
-    POP 0X7F 
-    POP 0X7E 
-    MOV A,#0X00
-    PUSH ACC
+    POP 0X7F            
+    POP 0X7E    
+    MOV A,#0X00                     ;if key element is not found 
+    PUSH ACC                        ;return 0x0000 
     PUSH ACC
     PUSH 0X7E 
     PUSH 0X7F  
-    RET
+    RET                             ;return subroutine 
 
 REPEAT_BINSEARCH:
     MOV A,R6 
-    MOVC A,@A+DPTR  ;Mov a <-- a[mid+base Addr.] 
-    MOV R7,A    ;temp store a[mid]
-    SUBB A,R0      ;Compare with key element 
+    MOVC A,@A+DPTR                  ;Mov a <-- a[mid+base Addr.] 
+    MOV R7,A                        ;temp store a[mid]
+    SUBB A,R0                       ;Compare with key element 
     JNZ NOTFOUND_BINSSEARCH
-;if key element is equal (Search is Sucessfull)
+;--------------if key element is equal (Search is Sucessfull)----------------
     POP 0X7F 
     POP 0X7E 
 
-    MOV A,R6    ;add mid into datapointer
+    MOV A,R6                        ;add mid into datapointer
     ADD A,DPL
-    JNC SKIP_BINSEARCH ;if c=1 inc dph 
+    JNC SKIP_BINSEARCH              ;if c=1 inc dph 
     INC DPH
 SKIP_BINSEARCH:
-    ; push addr. of key and return subroutine  
+                                    ;push addr. of key and return subroutine  
     PUSH DPL
     PUSH DPH
     PUSH 0X7E 
     PUSH 0X7F 
     RET 
 
-;if element is not found 
+;----------------------if element is not found-------------------------- 
 NOTFOUND_BINSSEARCH:
-    MOV A,R7    ;move a[mid] 
-    SUBB A,R0   ;compare a[mid] with key element 
+    MOV A,R7                        ;move a[mid] 
+    SUBB A,R0                       ;compare a[mid] with key element 
     JNC GREATER_BINSEARCH
-    INC R6      ;increment mid 
-    MOV A,R6 
-    MOV R4,A    ;move lo: = mid +1
+    INC R6                          ;increment mid 
+    MOV A,R6                    
+    MOV R4,A                        ;move lo: = mid +1
 
     MOV A,R4
     PUSH ACC 
     MOV A,R5 
     PUSH ACC
-    POP ACC     ;get mid into Accumulator
-    MOV R6,A    ;temp store mid 
+    POP ACC                         ;get mid into Accumulator
+    MOV R6,A                        ;temp store mid 
     AJMP START_BINSEARCH
 
 ;a[mid] is greater a[mid] > key 
 GREATER_BINSEARCH:
-    DEC R6      ;decrement mid  
-    MOV A,R6 
-    MOV R5,A    ;mov hi: = mid -1
+    DEC R6                          ;decrement mid  
+    MOV A,R6                    
+    MOV R5,A                        ;mov hi: = mid -1
 
 
     MOV A,R4
@@ -139,11 +136,11 @@ GREATER_BINSEARCH:
     PUSH ACC
 
     ACALL MIDCAL_BINSEARCH
-    POP ACC     ;get mid into Accumulator
-    MOV R6,A    ;temp store mid 
+    POP ACC                         ;get mid into Accumulator
+    MOV R6,A                        ;temp store mid 
     AJMP START_BINSEARCH
 
-; Mid calculation subroutine 
+;-------------------Mid calculation subroutine--------------------
 MIDCAL_BINSEARCH:
     POP 0X7F 
     POP 0X7E 
@@ -155,11 +152,9 @@ MIDCAL_BINSEARCH:
     
     ADD A,0X5F 
     MOV B,#0X02
-    DIV AB  ;(low + high)/2 
+    DIV AB                          ;(low + high)/2 
 
     PUSH ACC 
     PUSH 0X7E
     PUSH 0X7F
     RET 
-
-
